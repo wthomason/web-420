8a0f55bb2fb10ecc932aa3ea51476b2b15c9ea13
wthomason <williamthomason92@yahoo.com>
2019-05-05 16:28:54 -0500

    added atribution

--- api-gateway/config.js
+++ api-gateway/config.js
@@ -1,3 +1,13 @@
+/*
+============================================
+; Title:  config
+; File Name: config.js
+; Author: William Thomason
+; Date:   3 May 2019
+; Description: config.js file
+;===========================================
+*/
+
 var config = {};
 config.web = {};
 config.web.port = process.env.PORT || "3000";
