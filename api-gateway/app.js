7e3fbcfe2d965644aeb42428e7d80155f417edda
wthomason <williamthomason92@yahoo.com>
2019-05-12 22:09:07 -0500

    2.2

--- api-gateway/app.js
+++ api-gateway/app.js
@@ -10,7 +10,7 @@
 
 //HEADER
 const header = require("../thomason-header.js");
-console.log(header.display("William", "Thomason", "EMS") + "\n");
+console.log(header.display("William", "Thomason", "api-gateway") + "\n");
 
 //PROGRAM START
 
